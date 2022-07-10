10f7b248e7605945aa7fe819c0643432afa49c63
bxxloob_-@naver.com <nanaukim@naver.com>
2022-07-10 18:27:33 +0900

    0710 로그인페이지 (기능미구현)

---
+++ project/src/components/SignupForm.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SignupForm = () => {
+
+  return(
+    <SignupBody>
+      <div>여기가 회원가입 페이지지롱</div>
+    </SignupBody>
+  )
+
+}
+
+const SignupBody = styled.div`
+width : 100vw;
+height : 65vh;
+border : 1px solid #555;
+
+
+`
+
+export default SignupForm;
