83c0d7cad63f55d73c1ed70021a4eb3190949edd
JaewonyH <ghkd6535@naver.com>
2022-08-04 14:49:46 +0900

    cookie_false

--- project/src/axios.js
+++ project/src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const Domain = "http://valuetogether.tk"
 
-axios.defaults.withCredentials = true; // 쿠키 데이터를 전송받기 위해
+// axios.defaults.withCredentials = true; // 쿠키 데이터를 전송받기 위해
 export const request = (method, url, data) => {
   return axios({
     method,
