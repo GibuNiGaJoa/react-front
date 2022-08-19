aab94b760ef116abf568ebe23ab4bbe3e656b957
JaewonyH <ghkd6535@naver.com>
2022-08-19 16:06:55 +0900

    공유기능 완료

--- project/src/components/ShareModal.js
+++ project/src/components/ShareModal.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { useScript } from "../hook";
 import {
     FacebookShareButton,
     FacebookIcon,
@@ -7,6 +8,7 @@ import {
     TwitterShareButton,
 } from "react-share";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import kakaoLogo from "../img/kakao.png"
 
 
 const ShareModal = (props) => {
@@ -15,6 +17,25 @@ const ShareModal = (props) => {
         // console.log('닫힘버튼 실행됨')
     }
     const currentUrl = window.location.href;
+    // kakao SDK import하기
+	const status = useScript("https://developers.kakao.com/sdk/js/kakao.js");
+	
+	// kakao sdk 초기화하기
+	// status가 변경될 때마다 실행되며, status가 ready일 때 초기화를 시도합니다.
+	useEffect(() => {
+		if (status === "ready" && window.Kakao) {
+			// 중복 initialization 방지
+			if (!window.Kakao.isInitialized()) {
+				// 두번째 step 에서 가져온 javascript key 를 이용하여 initialize
+				window.Kakao.init("ff97517dba277b039555c6db19670343");
+			}
+		}
+	}, [status]);
+    const handleKakaoButton = () => {
+        window.Kakao.Link.sendScrap({
+            requestUrl: currentUrl,
+        });
+    };
 
 
     return (
@@ -32,7 +53,7 @@ const ShareModal = (props) => {
                     <CopyToClipboard text={currentUrl}>
                         <URLShareBtn>URL</URLShareBtn>
                     </CopyToClipboard>
-                    <button>카카오API</button>
+                    <KakaoShareBtn onClick={handleKakaoButton}><KakaoIcon src={kakaoLogo}></KakaoIcon></KakaoShareBtn>
                 </ShareBtns>
             </ModalContent>
         </ModalContainer>
@@ -42,6 +63,18 @@ const ShareModal = (props) => {
 };
 
 const ShareBtns = styled.div``;
+
+const KakaoShareBtn = styled.button`
+width: 48px;
+height: 48px;
+color: white;
+border-radius: 24px;
+border: 0px;
+cursor: pointer;`;
+const KakaoIcon = styled.img`
+width: 48px;
+height: 48px;
+`;
 
 const URLShareBtn =styled.button`
 width: 48px;
